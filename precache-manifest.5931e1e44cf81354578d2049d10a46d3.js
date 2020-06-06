79f7523e603d872686680bb38388543d5b728094
jeff-niemann <>
2020-06-06 16:00:16 -0600

    Updates

---
+++ precache-manifest.5931e1e44cf81354578d2049d10a46d3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a0d426aa07d488e80e0541a594fd6af",
+    "url": "/balance-board-tracker/index.html"
+  },
+  {
+    "revision": "a148afbe639c26c7bee4",
+    "url": "/balance-board-tracker/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "52787c268cf92de502a1",
+    "url": "/balance-board-tracker/static/js/2.4283fe58.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/balance-board-tracker/static/js/2.4283fe58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a148afbe639c26c7bee4",
+    "url": "/balance-board-tracker/static/js/main.ed886b32.chunk.js"
+  },
+  {
+    "revision": "08eb21dc83ff7c8c256e",
+    "url": "/balance-board-tracker/static/js/runtime-main.101ddc3e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/balance-board-tracker/static/media/logo.5d5d9eef.svg"
+  }
+]);
